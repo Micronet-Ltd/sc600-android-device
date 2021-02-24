ed7211f7c1706158011e80ed587b0b41cc8c25de
vladimirz <vladimir.zatulovsky@micronet-inc.com>
2021-02-24 15:23:18 +0200

    certification, lights and permissions services

--- qcom/msm8953_64/msm8953_64.mk
+++ qcom/msm8953_64/msm8953_64.mk
@@ -100,10 +100,10 @@ ifeq ($(PRODUCT_VARIANT),smartcam)
 PRODUCT_MODEL := MSCAM
 DEVICE_NAME   := MSCAM
 ifeq ($(PRODUCT_EXT_APK),lm)
-PRODUCT_VER    := 11.0.0.5
+PRODUCT_VER    := 11.0.0.6
 PRODUCT_EXT_APK := lm
 else
-PRODUCT_VER    := 10.0.0.5
+PRODUCT_VER    := 10.0.0.6
 PRODUCT_EXT_APK :=
 endif
 PRODUCT_VARIANT := smartcam
@@ -117,11 +117,11 @@ else
 PRODUCT_MODEL := SmarTab-8
 DEVICE_NAME   := SmarTab-8
 ifeq ($(TARGET_BUILD_VARIANT),user)
-PRODUCT_VER    := 01.0.0.5
+PRODUCT_VER    := 01.0.0.6
 PRODUCT_GMS_COMMON := true
 DISPLAY_BUILD_NUMBER := true
 else
-PRODUCT_VER    := 00.0.0.5
+PRODUCT_VER    := 00.0.0.6
 PRODUCT_GMS_COMMON ?= false
 endif
 endif
