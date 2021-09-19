95c84b44c8679e77eb5fe18d3200c6d8959ecf19
vladimirz <vladimir.zatulovsky@micronet-inc.com>
2021-09-19 10:57:01 +0300

    add modem config app

--- qcom/msm8953_64/msm8953_64.mk
+++ qcom/msm8953_64/msm8953_64.mk
@@ -103,16 +103,16 @@ PRODUCT_MODEL := MSCAM
 DEVICE_NAME   := MSCAM
 ifeq ($(PRODUCT_EXT_APK),lm)
 ifeq ($(PRODUCT_RB_OTA), enabled)
-PRODUCT_VER    := 11.0.1.17
+PRODUCT_VER    := 11.0.1.19
 else
-PRODUCT_VER    := 31.0.1.17
+PRODUCT_VER    := 31.0.1.19
 endif
 PRODUCT_EXT_APK := lm
 else
 ifeq ($(PRODUCT_RB_OTA), enabled)
-PRODUCT_VER    := 10.0.1.17
+PRODUCT_VER    := 10.0.1.19
 else
-PRODUCT_VER    := 30.0.1.17
+PRODUCT_VER    := 30.0.1.19
 endif
 PRODUCT_EXT_APK :=
 endif
@@ -128,17 +128,17 @@ PRODUCT_MODEL := SmarTab-8
 DEVICE_NAME   := SmarTab-8
 ifeq ($(TARGET_BUILD_VARIANT),user)
 ifeq ($(PRODUCT_RB_OTA), enabled)
-PRODUCT_VER    := 01.0.1.17
+PRODUCT_VER    := 01.0.1.19
 else
-PRODUCT_VER    := 21.0.1.17
+PRODUCT_VER    := 21.0.1.19
 endif
 PRODUCT_GMS_COMMON := true
 DISPLAY_BUILD_NUMBER := true
 else
 ifeq ($(PRODUCT_RB_OTA), enabled)
-PRODUCT_VER    := 00.0.1.17
+PRODUCT_VER    := 00.0.1.19
 else
-PRODUCT_VER    := 20.0.1.17
+PRODUCT_VER    := 20.0.1.19
 endif
 PRODUCT_GMS_COMMON ?= false
 endif
@@ -491,5 +491,5 @@ PRODUCT_PACKAGES += iodriver recovery.iodriver populate_board_id.sh
 ifeq ($(PRODUCT_EXT_APK),lm)
 PRODUCT_PACKAGES += lm.smartcam.androidapp libLMLibEncDec libLMLibJni liblocee liblocee-jni 
 endif
-
+PRODUCT_PACKAGES += modemconfigapp
 PRODUCT_PACKAGES += bootanimation.zip
